39f35a4bea81f2628e75ba45cc84141442c00a68
imp <106537315+imptype@users.noreply.github.com>
2023-05-05 18:06:07 +0100

    added code for local host

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+import os
+import json
+
+# Load configs for local hosting
+try:
+  with open('config.json') as f: 
+    config = json.loads(f.read())
+  for key, value in config.items():
+    os.environ[key] = value
+except:
+  pass
+
+# Validate pass
+if os.getenv('PASS') != '1234':
+  exit()
+
 from src.bot import run
 
 print('''
