bf0d9cf092ca067a9bbc1cd082fec5398d902869
imptype <106537315+imptype@users.noreply.github.com>
2024-05-10 21:23:48 +0000

    update top

--- src/screens/top.py
+++ src/screens/top.py
@@ -115,7 +115,7 @@ class TopView(discohook.View):
         if y < size[1]: # within bounds of resized grid
           for x in grid[y]:
             print(x)
-            if int(x) < size[0] and ((not reset and len(grid[y][x]) == configs['count']) or grid[y][x][-1] == reset): # within bounds v2
+            if int(x) < size[0] and (not configs['local'] or ((not reset and len(grid[y][x]) == configs['count']) or grid[y][x][-1] == reset)): # within bounds v2
               if flag:
                 if flag == 2: # [0 color, 1 timestamp, 2 count, 3 userid/None when local canvas, 4 guildid/None when local canvas or global canvas dms]
                   guilds[grid[y][x][4]] += 1
