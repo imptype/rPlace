7f2239bc3f4b8206288ff3b0b7283db98ea61c9d
imptype <106537315+imptype@users.noreply.github.com>
2024-05-14 16:12:31 +0000

    improve preview command

--- src/cogs/preview.py
+++ src/cogs/preview.py
@@ -2,7 +2,7 @@ import io
 import asyncio
 import discohook
 from PIL import Image
-from ..utils.helpers import get_grid, draw_map
+from ..utils.helpers import get_grid, draw_map, get_user_data, get_guild_data
 from ..utils.constants import COLOR_BLURPLE, IMAGE_SIZE, CANVAS_SIZE, COLOR_ORANGE
 
 @discohook.command.slash('preview', description = 'Preview another server\'s canvas!',
@@ -33,6 +33,46 @@ async def preview_command(interaction, server_id):
   if grid: # grid exists
     size = configs.get('size') or CANVAS_SIZE
 
+    tile = next(iter(next(iter(grid.values())).values()))
+
+    if len(tile) > 3 and isinstance(tile[-1], int): # ignore reset values
+      offset = 1
+    else:
+      offset = 0
+    
+    n = len(tile) - offset
+
+    if n == 3:
+      name, icon = await get_user_data(interaction, server_id)
+      fmt = '{}'
+    elif n == 4:
+      name, icon = await get_guild_data(interaction, server_id)
+      icon = 'https://cdn.discordapp.com/icons/{}/{}.png'.format(server_id, icon) # icon hash
+    else:
+      raise ValueError('unexpected tile count', tile)
+
+    reset = configs.get('reset')
+    count = configs.get('count')
+
+    tile_count = sum(
+      1
+      for y in grid
+      if y < size[1]
+      for x in grid[y]
+      if int(x) < size[0] and ((not reset and len(grid[y][x]) == count) or grid[y][x][-1] == reset)
+    )
+    
+    embed = discohook.Embed(
+     '{}\'s Canvas'.format(name),
+      description = '\n'.join([
+        'Canvas size: {}x{}'.format(*size),
+        '',
+        'Pixels filled: {}%'.format('{:.2f}'.format((tile_count / (size[0] * size[1])) * 100).rstrip('0').removesuffix('.'))
+      ]),
+      color = COLOR_BLURPLE
+    )
+    embed.set_thumbnail(icon)
+
     def blocking():
       im = draw_map(grid, configs)
       factor = IMAGE_SIZE // max(size)
@@ -43,14 +83,7 @@ async def preview_command(interaction, server_id):
       im.save(buffer, 'PNG')
       return buffer
 
-    # draw canvas
     buffer = await asyncio.to_thread(blocking)
-
-    embed = discohook.Embed(
-      'Canvas: {}'.format(server_id),
-      description = 'Canvas size: {}x{}'.format(*size),
-      color = COLOR_BLURPLE
-    )
     embed.set_image(discohook.File('map.png', content = buffer.getvalue()))
 
   else:
